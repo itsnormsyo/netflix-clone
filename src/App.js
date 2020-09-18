5682be50a692eef5d09cd1b62788c30d15a68481
itsnormsyo <itsnormsyo@gmail.com>
2020-09-18 05:10:40 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-      {/* navbar */}
-      {/* banner */}
-
+      <Nav />
       <Banner />
 
       <Row
